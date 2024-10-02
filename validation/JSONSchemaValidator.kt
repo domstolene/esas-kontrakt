ca63d0ebdd19fd5754f85247c81991885e3a0f5c
brigittbright <brigittbright@gmail.com>
2024-10-02 10:39:05 +0200

    Refactor JSON schema loading logic
    Simplify the schema loading method by encapsulating it within a single function. Enhance the validation error handling by introducing distinct and more informative error messages.

--- validation/JSONSchemaValidator.kt
+++ validation/JSONSchemaValidator.kt
@@ -1,4 +1,3 @@
-import java.lang.Class
 import net.pwall.json.schema.JSONSchema
 import net.pwall.json.schema.JSONSchemaException
 import java.io.InputStream
@@ -28,26 +27,57 @@ value class JsonSchemaString(val value: String)
  * @param clazz The class for which the JSON schema is defined.
  */
 class JSONSchemaValidator(schemaUri: URI) {
-    private val schemaCache = HashMap<URI, JsonSchemaString>()
-    private var schema: JsonSchemaString = schemaCache.getOrPut(schemaUri) {
-        val schemaStream: InputStream = this::class.java.classLoader.getResourceAsStream(schemaUri.toString())
-            ?: throw JSONSchemaException("Something went wrong while reading from $schemaUri")
+    private val schema = loadSchema(schemaUri.toString())
 
-        schemaStream.use {
-            val jsonSchemaAsString = it.bufferedReader(Charsets.UTF_8).readText()
-            JsonSchemaString(jsonSchemaAsString)
+    private fun loadSchema(schemaPath: String): JsonSchemaString {
+        val schemaStream: InputStream = this::class.java.classLoader.getResourceAsStream(schemaPath)
+            ?: throw JSONSchemaException("Something went wrong while reading from $schemaPath")
+
+        schemaStream.bufferedReader(Charsets.UTF_8).use {
+            val jsonSchemaAsString = it.readText()
+            if (jsonSchemaAsString.isBlank()) {
+                throw JSONSchemaException("Loaded schema is empty at $schemaPath")
+            }
+
+            return JsonSchemaString(jsonSchemaAsString)
         }
     }
 
     fun validateOrThrow(data: String): String {
         val jsonSchema: JSONSchema = JSONSchema.parse(schema.value)
-        val errorMessage = jsonSchema.validateBasic(data)
+        val validationResult = jsonSchema.validateBasic(data)
 
-        errorMessage.takeIf { error -> !error.valid }?.let {
+        validationResult.takeIf { res -> !res.valid }?.let {
             val errors = it.errors?.map { err -> SchemaValidationError(err.error, err.instanceLocation) }
-            throw FailedValidationException("Validation failed: $errors}")
+                ?.distinctError()
+            throw FailedValidationException("Validation failed: $errors")
         }
         return data
+    }
+
+    /**
+     * Processes a list of `SchemaValidationError` objects and returns a single formatted error message.
+     * The method groups errors by their location, filters out less specific ones, and ensures the uniqueness of
+     * error messages within the most specific location.
+     *
+     * @return A formatted string containing the most nested error messages occurred during validation.
+     */
+    private fun List<SchemaValidationError>.distinctError(): String {
+        val groupedErrors = this.groupBy { it.location }
+        val sorted = groupedErrors.entries.sortedByDescending { it.key.length }
+
+        val processedLocations = mutableSetOf<String>()
+        val uniqueErrors = sorted.flatMap { (location, errors) ->
+            if (processedLocations.none { location.startsWith(it) }) {
+                processedLocations.add(location)
+                errors.distinctBy { it.error }
+            } else emptyList()
+        }
+
+        // Format the errors
+        return uniqueErrors.groupBy { it.location }.map { (location, errs) ->
+            "error found at $location:\n" + errs.joinToString("\n") { "  - ${it.error}" }
+        }.joinToString("\n")
     }
 }
 
